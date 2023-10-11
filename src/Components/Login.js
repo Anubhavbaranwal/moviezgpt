3f624514ae3739c8c7b890ecc30e8f2108144774
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2023-10-11 09:34:24 +0530

    SignIn form created

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignForm, setIsSignForm] = useState(true);
+  const handletoogleform = () => {
+    setIsSignForm(!isSignForm);
+  };
   return (
     <div>
       <Header />
@@ -12,7 +16,18 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute bg-black p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl my-4">Sign In</h1>
+        {isSignForm ? (
+          <h1 className="font-bold text-3xl my-4">Sign In</h1>
+        ) : (
+          <h1 className="font-bold text-3xl my-4">Sign Up</h1>
+        )}
+        {!isSignForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-800"
+          />
+        )}
         <input
           type="text"
           placeholder="Email"
@@ -24,8 +39,17 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-800"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignForm ? "Sign In" : "Sign Up"}
         </button>
+        {isSignForm ? (
+          <p className="py-4 cursor-pointer" onClick={handletoogleform}>
+            New to Netfilx ? Sign Up
+          </p>
+        ) : (
+          <p className="py-4 cursor-pointer" onClick={handletoogleform}>
+            Already a User ? Sign In
+          </p>
+        )}
       </form>
     </div>
   );
