891eba2804d7f541cf8644ef7aad762686f0ced5
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2023-10-13 21:14:28 +0530

    validate email

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validate } from "../utils/Validate";
 
 const Login = () => {
   const [isSignForm, setIsSignForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleButtonClick = () => {
+    //validate the form data
+    const message = validate(email.current.value, password.current.value);
+    setErrorMessage(message);
+    console.log(message);
+  };
+  console.log(email);
   const handletoogleform = () => {
     setIsSignForm(!isSignForm);
   };
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="absolute bg-gradient-to-b from-black">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/893a42ad-6a39-43c2-bbc1-a951ec64ed6d/1d86e0ac-428c-4dfa-9810-5251dbf446f8/IN-en-20231002-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg"
         />
       </div>
-      <form className="w-3/12 absolute bg-black p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        {isSignForm ? (
-          <h1 className="font-bold text-3xl my-4">Sign In</h1>
-        ) : (
-          <h1 className="font-bold text-3xl my-4">Sign Up</h1>
-        )}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute bg-black p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        onClick={handleButtonClick}
+      >
+        <h1 className="font-bold text-3xl my-4">
+          {isSignForm ? "Sign In" : "Sign Up"}
+        </h1>
         {!isSignForm && (
           <input
             type="text"
@@ -29,27 +42,27 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email"
           className="p-4 my-4 w-full bg-gray-800"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        <p className="text-red-500">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          // onClick={handletoogleform}
+        >
           {isSignForm ? "Sign In" : "Sign Up"}
         </button>
-        {isSignForm ? (
-          <p className="py-4 cursor-pointer" onClick={handletoogleform}>
-            New to Netfilx ? Sign Up
-          </p>
-        ) : (
-          <p className="py-4 cursor-pointer" onClick={handletoogleform}>
-            Already a User ? Sign In
-          </p>
-        )}
+        <p className="py-4 cursor-pointer" onClick={handletoogleform}>
+          {isSignForm ? "New to Netfilx ? Sign Up" : "Already a User ? Sign In"}
+        </p>
       </form>
     </div>
   );
