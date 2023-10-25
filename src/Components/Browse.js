57f8089a832be44003f9dce9f58863d33a0fdffb
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2023-10-25 13:26:21 +0530

    api added

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_options } from "../utils/Constant";
 
 const Browse = () => {
+  const url = "https://api.themoviedb.org/3/movie/now_playing?page=1";
+
+  const getdata = async () => {
+    const data = await fetch(url, API_options);
+    const json = await data.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
   return (
     <div>
       <Header />
