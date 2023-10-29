69a502abab820522f9cc2823f501234efddde7f0
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2023-10-29 15:03:38 +0530

    Videotitle

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{overview}</p>
+    <div className="pt-36 px-12">
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="py-6 text-md w-1/3">{overview}</p>
+      <div>
+        <button className="text-black bg-white p-4 px-12 text-lg bg-opacity-50 rounded-lg ">
+          ▶️ Play
+        </button>
+        <button className="text-black bg-white mx-2 p-4 px-12 text-lg bg-opacity-50 rounded-lg ">
+          More Info
+        </button>
+      </div>
     </div>
   );
 };
